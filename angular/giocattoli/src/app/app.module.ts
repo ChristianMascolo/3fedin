f1e1ee2338a05150db0f7d0c81ca6c0b69a5b773
monicasebastiano05 <sebastianomonica@gmail.com>
2024-03-18 11:00:03 +0100

    fix repo

---
+++ angular/giocattoli/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GiocattoloComponent } from './giocattolo/giocattolo.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GiocattoloComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
